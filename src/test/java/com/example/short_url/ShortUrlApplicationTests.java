7830a2cf7b29f0bf675133e66e41449e5d3d607d
minyoung <myouth_u@naver.com>
2025-04-10 02:15:33 +0900

    application-test.yml 추가

--- src/test/java/com/example/short_url/ShortUrlApplicationTests.java
+++ src/test/java/com/example/short_url/ShortUrlApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.short_url;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // 테스트 코드 실행 시 test.yml 파일 사용
 class ShortUrlApplicationTests {
 
 	@Test
